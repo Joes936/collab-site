42f6e93cd3d888cc9bee1ff5e221accef09136d8
Joes936 <samcybersec@proton.me>
2025-06-11 11:50:56 +0300

    Update auto_push.ps1

--- auto_push.ps1
+++ auto_push.ps1
@@ -1,5 +1,1 @@
-Set-Location "C:\Users\samde\OneDrive\Desktop\collab-site"
-git add .
-$datetime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-git commit -m "Auto-sync $datetime"
-git push origin main
+
